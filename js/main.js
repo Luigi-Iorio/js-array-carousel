023b858093cb47a639b0b42078fb953ca7d31fb2
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-10 17:03:50 +0100

    feat: milestone 2

--- js/main.js
+++ js/main.js
@@ -35,3 +35,40 @@ for (let i = 0; i < immagini.length; i++) {
   // inserisco immagine in item
   item.append(img);
 }
+
+// creazione frecce
+// freccia in alto
+const alto = document.createElement("div");
+// assegnazione classe prev
+alto.classList.add("prev");
+// inserimento in items
+items.append(alto);
+// freccia in basso
+const basso = document.createElement("div");
+// assegnazione classe prev
+basso.classList.add("next");
+// inserimento in items
+items.append(basso);
+
+// domitems - per selezzionare tutti gli elementi item nel dom
+const domItems = document.querySelectorAll(".item");
+console.log(domItems);
+
+// evento per cambiare le immagini
+// scorrere in dietro
+alto.addEventListener("click", function () {
+  if (contImmagine > 0) {
+    domItems[contImmagine].classList.remove("active");
+    contImmagine--;
+    domItems[contImmagine].classList.add("active");
+  }
+});
+
+// scorrere in avanti
+basso.addEventListener("click", function () {
+  if (contImmagine < domItems.length - 1) {
+    domItems[contImmagine].classList.remove("active");
+    contImmagine++;
+    domItems[contImmagine].classList.add("active");
+  }
+});
